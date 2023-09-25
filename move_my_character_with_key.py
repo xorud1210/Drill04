649bcb15b5bb80f37653e51230f7277fa2e2778d
xorud1210 <rhxo1210@naver.com>
2023-09-25 22:13:27 +0900

    애니메이션 확인

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -1,5 +1,21 @@
 from pico2d import *
 
-open_canvas()
+TUK_WIDTH, TUK_HEIGHT = 1280, 1024
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
+tuk_ground = load_image('TUK_GROUND.png')
+character = load_image('Balrog.png')
 
-tuk_ground = load_image('')
+
+running = True
+frame = 0
+x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+
+while running:
+    clear_canvas()
+    tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    character.clip_draw(frame * 185, 0, 185, 175, x, y)
+    update_canvas()
+    frame = (frame + 1) % 5
+    delay(0.05)
+
+close_canvas()
